54ade74193b403058f7e4a28274078d97ebed1f1
chase <chase622@gmail.com>
2023-03-26 14:46:57 -0400

    5 down

---
+++ labs/newlab/paramiko-ng-2.7.6/sites/docs/conf.py
@@ -0,0 +1,51 @@
+import alabaster
+
+import sys
+from os.path import abspath, join, dirname
+sys.path.append(abspath(join(dirname(__file__), '..', '..')))
+
+extensions = [
+    'sphinx.ext.intersphinx',
+    'sphinx.ext.autodoc',
+    'alabaster',
+    'releases',
+]
+
+# Alabaster theme + mini-extension
+html_theme_path = [alabaster.get_path()]
+html_theme = 'alabaster'
+html_theme_options = {
+    'description': "A Python implementation of SSHv2.",
+    'github_user': 'ploxiln',
+    'github_repo': 'paramiko-ng',
+    'github_type': 'star',
+    'travis_button': True,
+}
+html_sidebars = {
+    '**': [
+        'about.html',
+        'navigation.html',
+        'searchbox.html',
+    ]
+}
+
+# Regular settings
+project = 'Paramiko-NG'
+copyright = 'Paramiko contributors'
+master_doc = 'index'
+templates_path = ['_templates']
+exclude_trees = ['_build']
+source_suffix = '.rst'
+default_role = 'obj'
+
+# Autodoc settings
+autodoc_default_flags = ['members', 'special-members']
+
+# Intersphinx connection to stdlib
+intersphinx_mapping = {
+    'python': ('https://docs.python.org/2.7/', None),
+}
+
+# just used for old changelog
+releases_github_path = "paramiko/paramiko"
+releases_document_name = ["old_changelog"]
